018e32a76335b4274310884982a6224c0e881ac0
Elizaveta Guzova <guzova.elizaveta00@gmail.com>
2022-10-27 22:32:07 +0600

    Add animals

--- LearnGit/Program.cs
+++ LearnGit/Program.cs
@@ -6,7 +6,8 @@ namespace LearnGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
